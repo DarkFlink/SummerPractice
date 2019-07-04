ed3ebe1a1cf8561a94edb7e10feeca8e9f366bca
DarkFlink <zond.999@yandex.ru>
2019-07-04 23:15:32 +0300

    project added

---
+++ VKFriends/src/Main.java
@@ -0,0 +1,27 @@
+import VKClient.VKClient;
+import VKClient.VKUser;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    private static final String[]  requestArgs = {"photo50", "education"};
+
+    public static void main(String[] args) {
+
+        VKClient vk = new VKClient();
+
+        // id of user
+        int userID = 1411555542;
+
+        // get json
+        String response = vk.getUserFriends(userID, requestArgs);
+        ArrayList<VKUser> list;
+        list = vk.parseFriendsJson(response);// json -> ArrayList
+
+        // this only for result
+        if (list != null)
+            for (VKUser el : list)
+                System.out.println(el.toString());
+    }
+}
