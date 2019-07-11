8985617246126289be31c006c3da3ab02f0012a6
Gizzatovamir <Gizzatovamir777@gmail.com>
2019-07-12 01:12:59 +0300

    UnitTests

--- tests/VKClient/VKClientTest.java
+++ tests/VKClient/VKClientTest.java
@@ -8,15 +8,16 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 public class VKClientTest {
     private static final String[] requestArgs = {"photo50", "education"};
+    public static final String[] basicArgs = {"id", "first_name", "last_name", "deactivated", "is_closed", "photo_50"};
     private int userID = 206043986;
     @Test
         public void parseFriendsJson() {
         VKClient user = new VKClient();
-        String response = user.getUserFriends(userID, requestArgs);
+        String response = user.getUserFriends(userID, basicArgs);
         ArrayList<VKUser> list = new ArrayList<>();
         list = user.parseFriendsJson(response);
         try {
-            File test = new File(new File("").getAbsolutePath()  + "\\tests\\VKClient\\test1.txt");
+            File test = new File(new File("").getAbsolutePath()  + "\\tests\\VKClient\\DataSets\\test1.txt");
             BufferedReader br;
             FileReader fileReader = new FileReader(test);
             br = new BufferedReader(fileReader);
@@ -49,12 +50,52 @@ public class VKClientTest {
     public void getUser() {
         VKClient user = new VKClient();
         userID = 206043986;
-        String response = user.getUserFriends(userID, requestArgs);
+        String response = user.getUserFriends(userID, basicArgs);
         ArrayList<VKUser> list;
         list = user.parseFriendsJson(response);
         for(int i = 0; i< list.size();i++){
-            VKUser tmp = list.get(i);
-            //System.out.println(user.getUser(tmp.userId,null));
+            try {
+                VKUser tmp = list.get(i);
+                String[] photo = {"photo_50"};
+                System.out.print(user.getUser(Integer.toString(tmp.userId), photo).toString() + " ");
+            }catch (Throwable e){
+                System.out.print("ERROR" + e);
+            }
+            System.out.println();
+
+        }
+    }
+
+    @Test
+    public void getCommonFriends() {
+        try {
+            userID = 206043986;
+            VKClient user = new VKClient();
+            File test = new File(new File("").getAbsolutePath()  + "\\tests\\VKClient\\DataSets\\test2.txt");
+            BufferedReader br;
+            FileReader fileReader = new FileReader(test);
+            br = new BufferedReader(fileReader);
+            if (!test.exists()) {
+                System.out.println("File doesn't exists");
+                return;
+            }
+            ArrayList<String> id = new ArrayList<>();
+            String str;
+            while( (str = br.readLine()) != null){
+                id.add(str);
+            }
+            int[] target = new int[id.size()];
+            for(int i =0;i < id.size();i++) {
+                target[i] = Integer.parseInt(id.get(i));
+            }
+            ArrayList<Integer> common = user.getCommonFriends(userID, target);
+            String[] photo = {"photo_50"};
+            for(int i = 0; i < common.size();i++){
+                System.out.println(user.getUser(Integer.toString(target[i]),photo) + " - " + user.getUser(Integer.toString(userID),photo) + " in common: " + common.get(i));
+            }
+        }
+        catch(Throwable e) {
+            System.out.println("ERROR: " + e);
         }
     }
 }
